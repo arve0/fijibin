1edf1c02828a64b5260fcd0eec2f747fdbef044b
arve0 <arve.seljebu@gmail.com>
2015-02-08 15:09:11 +0100

    use 64-bit version for 64-bit operating system

--- fijibin/__init__.py
+++ fijibin/__init__.py
@@ -13,9 +13,12 @@ BIN_FOLDER = os.path.join(HOME, '.bin')
 EXTRACT_FOLDER = os.path.join(BIN_FOLDER, 'Fiji.app')
 FIJI_ROOT = os.path.join(BIN_FOLDER, 'Fiji-' + FIJI_VERSION + '.app')
 BIN_NAMES = {
-    'Linux': os.path.join(FIJI_ROOT, 'ImageJ-linux32'),
-    'Darwin': os.path.join(FIJI_ROOT, 'Contents/MacOS/ImageJ-macosx'),
-    'Windows': os.path.join(FIJI_ROOT, 'ImageJ-win32.exe')
+    'Darwin64bit': os.path.join(FIJI_ROOT, 'Contents/MacOS/ImageJ-macosx'),
+    'Linux32bit': os.path.join(FIJI_ROOT, 'ImageJ-linux32'),
+    'Linux64bit': os.path.join(FIJI_ROOT, 'ImageJ-linux64'),
+    'Windows32bit': os.path.join(FIJI_ROOT, 'ImageJ-win32.exe'),
+    'Windows64bit': os.path.join(FIJI_ROOT, 'ImageJ-win64.exe')
 }
 
-BIN = BIN_NAMES[platform.system()]
+SYSTEM = platform.system() + platform.architecture()[0]
+BIN = BIN_NAMES[SYSTEM]
