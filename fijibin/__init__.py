a20e92f4d4f8b6d3414e1b1202aad481ba52c957
Arve Seljebu <arve.seljebu@gmail.com>
2015-03-18 17:18:49 +0100

    win with jre

--- fijibin/__init__.py
+++ fijibin/__init__.py
@@ -4,7 +4,8 @@ Latest Life-Line version of fiji for easy inclusion in Python projects.
 __all__ = []
 VERSION = '0.0.5'
 FIJI_VERSION = '20141125'
-URL = 'http://fiji.sc/downloads/Life-Line/fiji-nojre-20141125.zip'
+BASE_URL = 'http://fiji.sc/downloads/Life-Line/fiji-'
+END_URL = '-' + FIJI_VERSION + '.zip'
 
 import platform, os
 
@@ -20,5 +21,15 @@ BIN_NAMES = {
     'Windows64bit': os.path.join(FIJI_ROOT, 'ImageJ-win64.exe')
 }
 
+_unix_url = BASE_URL + 'nojre' + END_URL
+URLS = {
+    'Darwin64bit': _unix_url,
+    'Linux32bit': _unix_url,
+    'Linux64bit': _unix_url,
+    'Windows32bit': BASE_URL + 'win32' + END_URL,
+    'Windows64bit': BASE_URL + 'win64'  + END_URL
+}
+
 SYSTEM = platform.system() + platform.architecture()[0]
 BIN = BIN_NAMES[SYSTEM]
+URL = URLS[SYSTEM]
